1dcff3629bf2ba0c00a9e95c45dc8de9e3ad2d86
Phone Min Kyi <phoneminkyi2222@gmail.com>
2024-11-21 14:55:17 +0630

    Testing making own project with PHP

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include("./layout/header.php");
+include("./layout/hero.php");
+?>
+<section id="about">
+    <?php include("./layout/about.php");
+?>
+</section>
+
+<section id="client">
+    <?php include("./layout/client.php");
+?>
+</section>
+
+<section id="feature">
+    <?php include("./layout/feature.php");
+?>
+</section>
+
+<section id="info">
+    <?php include("./layout/info.php");
+?>
+</section>
+
+<section id="professional">
+    <?php include("./layout/professional.php");
+?>
+</section>
+
+<section id="service">
+    <?php include("./layout/service.php");
+?>
+</section>
+
+
+<?php include("./layout/footer.php")?>;
